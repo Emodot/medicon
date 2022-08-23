1691f5b7c0c837af897dfa79dc85881bcd118c63
David Emaye <emayeodavid@gmail.com>
2022-08-23 01:49:28 +0100

    developed appointment page

---
+++ pages/appointments.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <p class="page-title">
+      Appointments
+    </p>
+    <div class="dashboard-table">
+      <TablesAppointmentTable />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'MainLayout'
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 50px;
+}
+
+.dashboard-table {
+  margin-top: 5rem;
+}
+
+@media only screen and (max-width: 500px) {
+  .container {
+    padding: 20px;
+  }
+}
+</style>
