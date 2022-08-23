2269b62b6fd32431c595932f826b6a634199c320
David Emaye <emayeodavid@gmail.com>
2022-08-23 01:56:59 +0100

    developing doctors page

---
+++ pages/doctors.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <p class="page-title">
+      Doctors
+    </p>
+    <div class="dashboard-table">
+      <TablesDoctorsTable />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'MainLayout'
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 50px;
+}
+
+.dashboard-table {
+  margin-top: 5rem;
+}
+
+@media only screen and (max-width: 500px) {
+  .container {
+    padding: 20px;
+  }
+}
+</style>
