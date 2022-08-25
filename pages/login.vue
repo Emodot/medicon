498bfba37c17d55380240d3ac741002596ddbff0
David Emaye <emayeodavid@gmail.com>
2022-08-25 03:28:41 +0100

    made fixes

---
+++ pages/login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="inner">
+      <div class="lhs">
+        <div class="logo">
+          <img src="~assets/images/medicon-logo.png" alt="">
+        </div>
+        <p class="title">
+          Sign In
+        </p>
+        <p class="sub-title">
+          Sign in to access your Account
+        </p>
+      </div>
+      <div class="rhs" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.inner {
+  display: flex;
+  height: 100vh;
+}
+
+.lhs {
+  flex-basis: 35%;
+  padding: 70px 50px;
+}
+
+.rhs {
+  flex-basis: 65%;
+  background-image: url('assets/images/login-bg.jpg');
+  background-size: cover;
+}
+</style>
