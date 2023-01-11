7a95dc0abf2a9a19007330eb8c4e99b3e71fbad5
David Emaye <emayeodavid@gmail.com>
2023-01-11 04:47:00 +0100

    changed font

--- components/Loader.vue
+++ components/Loader.vue
@@ -6,14 +6,14 @@
 
 <style scoped>
 .loader-ctn {
-  width: 100%;
+  /* width: 100%; */
 }
 .loader {
-  border: 3px solid #f3f3f3;
+  border: 2.5px solid #f2f3f3;
   border-radius: 50%;
-  border-top: 3px solid #000;
-  width: 20px;
-  height: 20px;
+  border-top: 2.5px solid #1D692C;
+  width: 18px;
+  height: 18px;
   -webkit-animation: spin 0.6s linear infinite;
   animation: spin 0.6s linear infinite;
   margin: 0 auto;
